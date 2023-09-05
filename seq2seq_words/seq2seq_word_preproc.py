e57a3349ad9d735aa79ce113d6fb7bfd17d59fc3
Jayanti Prasad <jayanti.prasad@disys.com>
2023-09-05 20:12:56 +0530

    minor changes

--- seq2seq_words/seq2seq_word_preproc.py
+++ seq2seq_words/seq2seq_word_preproc.py
@@ -35,34 +35,3 @@ def text2vec (input_text, df_vocab, vocab_size, vec_len):
     return text_vecs     
 
 
-
-if __name__ == "__main__":
-
-     input_voc_size = 10000
-     output_voc_size = 10000
-     
-     input_file = r"C:\Users\jayanti.prasad\Data\seq2seq_data\fra.txt"
-     
-     with open(input_file, 'r', encoding='utf-8') as f:
-         lines = f.read().split('\n')
-
-     input_texts = []
-     output_texts = []
-
-     for i in range (1, 1000):
-         row = lines[i].split('\t')
-         input_texts.append (row[0])
-         output_texts.append ("__START__ " + row[1] + " __STOP__")
-
-     V =  build_vocabulary(input_texts, output_texts)
-     V[0].to_csv("input_vocab.csv")
-     V[1].to_csv("output_vocab.csv")
-     
-
-     input_vecs  = text2vec (input_texts, V[0], 10000, 10)
-     output_vecs = text2vec (output_texts, V[1], 10000, 10)
-
-     for i in range (0, len(input_vecs)):
-          print(i, input_vecs[i], output_vecs[i])
-          
-                  
